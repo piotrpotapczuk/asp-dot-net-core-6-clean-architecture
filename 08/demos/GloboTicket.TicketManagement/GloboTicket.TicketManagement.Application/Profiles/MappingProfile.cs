6c92ba5f060310f3968b0af7ea91cb55b362d5e0
piotr.potapczuk <piotr.potapczuk@pkoleasing.pl>
2025-01-06 22:15:24 +0100

    init project from zip archive

---
+++ 08/demos/GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCateogry;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.UpdateEvent;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using GloboTicket.TicketManagement.Application.Features.Orders.Queries.GetOrdersForMonth;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryListVm>().ReverseMap();
+            CreateMap<Category, CategoryEventListVm>().ReverseMap();
+            CreateMap<Category, CreateCategoryCommand>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
+            CreateMap<Event, EventExportDto>().ReverseMap();
+
+            CreateMap<Event, CreateEventCommand>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
+
+            CreateMap<Order, OrdersForMonthDto>();
+        }
+    }
+}
---
+++ Assets/m4/event command/DeleteEvent/DeleteEventCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.DeleteEvent
+{
+    public class DeleteEventCommand : IRequest
+    {
+        public Guid EventId { get; set; }
+    }
+}
